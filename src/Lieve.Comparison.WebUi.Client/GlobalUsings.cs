b0773af9b7409d3696dc7805b31f07dc420f6b09
Mohammad Mohabbati <mohabbati@live.com>
2024-02-18 16:35:04 +0100

    Added global usings

---
+++ src/Lieve.Comparison.WebUi.Client/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using FluentValidation;
+global using Lieve.Comparison.Domain.Shared.Enums;
+global using Lieve.Comparison.WebUi.Client.Services.Interfaces;
+global using Microsoft.AspNetCore.Components;
+global using MudBlazor;
