caeb6abb31f7f9ed4d84a6c31302028193e78253
Mohammad Mohabbati <mohabbati@live.com>
2024-03-03 19:30:25 +0100

    Enhanced daterange picker

--- src/Lieve.Comparison.WebUi.Client/Components/LieveDateRangePicker/LieveDateRangePickerComponent.razor.cs
+++ src/Lieve.Comparison.WebUi.Client/Components/LieveDateRangePicker/LieveDateRangePickerComponent.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Reflection;
 
 namespace Lieve.Comparison.WebUi.Client.Components.LieveDateRangePicker;
 
@@ -22,21 +23,65 @@ public partial class LieveDateRangePickerComponent
     public EventCallback<DateRange> DateRangeChanged { get; set; }
 
     private MudDateRangePicker _dateRangePicker = default!;
-    private CultureInfo _cultureInfo = new("fa-IR");
-    private DateTime? lastPicker;
+    private CultureInfo _cultureInfo = GetPersianCulture();
+    private string _titleDateFormat = "dddd, dd MMMM,yyyy";
+    private DateTime? _lastPicker;
 
     private void ChangeCulture()
     {
-        lastPicker ??= _dateRangePicker.PickerMonth ?? DateTime.Now;
+        _lastPicker ??= _dateRangePicker.PickerMonth ?? DateTime.Now;
 
         if (_cultureInfo.Name == "fa-IR")
         {
             _cultureInfo = new CultureInfo("en-US");
+            _titleDateFormat = "ddd, dd MMM";
         }
         else
         {
-            _cultureInfo = new CultureInfo("fa-IR");
+            _cultureInfo = GetPersianCulture();
+            _titleDateFormat = "dddd, dd MMMM,yyyy";
         }
-        lastPicker = null;
+        _lastPicker = null;
+    }
+
+    private static CultureInfo GetPersianCulture()
+    {
+        var culture = new CultureInfo("fa-IR");
+        var formatInfo = culture.DateTimeFormat;
+
+        formatInfo.AbbreviatedDayNames = ["ی", "د", "س", "چ", "پ", "ج", "ش"];
+        formatInfo.DayNames = ["یکشنبه", "دوشنبه", "سه شنبه", "چهار شنبه", "پنجشنبه", "جمعه", "شنبه"];
+        
+        var monthNames = new[]
+        {
+            "فروردین", "اردیبهشت", "خرداد",
+            "تیر", "مرداد", "شهریور",
+            "مهر", "آبان", "آذر",
+            "دی", "بهمن", "اسفند",
+            "",
+        };
+
+        formatInfo.AbbreviatedMonthNames =
+            formatInfo.MonthNames =
+                formatInfo.MonthGenitiveNames = formatInfo.AbbreviatedMonthGenitiveNames = monthNames;
+        formatInfo.AMDesignator = "ق.ظ";
+        formatInfo.PMDesignator = "ب.ظ";
+        formatInfo.ShortDatePattern = "yyyy/MM/dd";
+        formatInfo.LongDatePattern = "dddd, dd MMMM,yyyy";
+        formatInfo.FirstDayOfWeek = DayOfWeek.Saturday;
+
+        var calendar = new PersianCalendar();
+        var fieldInfo = culture.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        
+        fieldInfo?.SetValue(culture, calendar);
+        
+        var info = formatInfo.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        
+        info?.SetValue(formatInfo, calendar);
+        culture.NumberFormat.NumberDecimalSeparator = "/";
+        culture.NumberFormat.DigitSubstitution = DigitShapes.NativeNational;
+        culture.NumberFormat.NumberNegativePattern = 0;
+        
+        return culture;
     }
 }
